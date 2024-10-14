0cc1e4c03a9a302431266a4a3c7341f3ce6b8522
OuYangLiang <ouyanggod@gmail.com>
2024-10-14 20:29:41 +0800

    more columns

--- src/views/guidaoheng/TableComp.vue
+++ src/views/guidaoheng/TableComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-table :columns="columns" :data-source="tableData.records" :pagination="pagination" :loading="loading" @change="handleTableChange" size="small" bordered>
+  <a-table :columns="columns" :scroll="{ x: 1500, y: 300 }" :data-source="tableData.records" :pagination="pagination" :loading="loading" @change="handleTableChange" size="small" bordered>
       <!-- <template #bodyCell="{ column, record }">
           <template v-if="column.dataIndex === 'zmxdocNo'">
               <a @click.prevent="viewDetail(record)">{{ record.zmxdocNo }}</a>
@@ -40,32 +40,139 @@ const queryData = () => axios(ajaxParam);
 
 const columns = [
   {
-    title: '明细磅单号',
-    dataIndex: 'zmxdocNo',
-    align: 'center'
-  },
-  {
-    title: '所属二级公司',
-    dataIndex: 'ssejdw',
-    align: 'center'
-  },
-  {
-    title: '所属三级公司',
-    dataIndex: 'sssjdw',
-    align: 'center'
-  },
-  {
-    title: '上报状态',
-    dataIndex: 'uploadStatus',
-    align: 'center'
-  },
-  {
     title: '操作',
+    width: 200,
     dataIndex: '',
     align: 'center',
     slots: {
       customRender: 'action',
     },
+  },
+  {
+    title: '明细磅单号',
+    dataIndex: 'zmxdocNo',
+    width: 100,
+    align: 'center'
+  },
+  {
+    title: '上报状态',
+    width: 100,
+    dataIndex: 'uploadStatus',
+    align: 'center'
+  },
+  {
+    title: '所属二级公司',
+    dataIndex: 'ssejdw',
+    width: 100,
+    align: 'center'
+  },
+  {
+    title: '所属三级公司',
+    width: 100,
+    dataIndex: 'sssjdw',
+    align: 'center'
+  },
+  {
+    title: '车次',
+    width: 100,
+    dataIndex: 'zccheci',
+    align: 'center'
+  },
+  {
+    title: '车型',
+    width: 100,
+    dataIndex: 'chexing',
+    align: 'center'
+  },
+  {
+    title: '煤源矿点名称',
+    width: 100,
+    dataIndex: 'mykuangdianmc',
+    align: 'center'
+  },
+  {
+    title: '煤源矿点编码',
+    width: 100,
+    dataIndex: 'mykuangdianbm',
+    align: 'center'
+  },
+  {
+    title: '首车号',
+    width: 100,
+    dataIndex: 'shouchehao',
+    align: 'center'
+  },
+  {
+    title: '尾车号',
+    width: 100,
+    dataIndex: 'weichehao',
+    align: 'center'
+  },
+  {
+    title: '车数',
+    width: 100,
+    dataIndex: 'cheshu',
+    align: 'center'
+  },
+  {
+    title: '计量单位',
+    width: 100,
+    dataIndex: 'jiliangdanwei',
+    align: 'center'
+  },
+  {
+    title: '总票重',
+    width: 100,
+    dataIndex: 'zongpiaozhong',
+    align: 'center'
+  },
+  {
+    title: '总毛重',
+    width: 100,
+    dataIndex: 'zongmaozhong',
+    align: 'center'
+  },
+  {
+    title: '总皮重',
+    width: 100,
+    dataIndex: 'zongpizhong',
+    align: 'center'
+  },
+  {
+    title: '总净重',
+    width: 100,
+    dataIndex: 'zongjingzhong',
+    align: 'center'
+  },
+  {
+    title: '总扣吨量',
+    width: 100,
+    dataIndex: 'koudunzongliang',
+    align: 'center'
+  },
+  {
+    title: '结算煤量',
+    width: 100,
+    dataIndex: 'jiesuanmeiliang',
+    align: 'center'
+  },
+  {
+    title: '总盈亏',
+    width: 100,
+    dataIndex: 'zongyingkui',
+    align: 'center'
+  },
+  {
+    title: '总盈吨',
+    width: 100,
+    dataIndex: 'zongyingdun',
+    align: 'center'
+  },
+  {
+    title: '总运损',
+    width: 100,
+    dataIndex: 'zongyunsun',
+    align: 'center'
   }
 ];
 
