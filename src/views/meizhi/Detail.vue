b9da57e3fa386ec405819e8cdb567271453054dd
OuYangLiang <ouyanggod@gmail.com>
2024-11-11 23:28:40 +0800

    tidies up codes.

--- src/views/meizhi/Detail.vue
+++ src/views/meizhi/Detail.vue
@@ -8,7 +8,7 @@
             <a-descriptions-item label="所属三级公司代码">{{ props.detailControl.record.sssjdwid }}</a-descriptions-item>
         </a-descriptions>
 
-        <a-table :columns="columns" :data-source="props.detailControl.record.dtData"  size="small" bordered />
+        <a-table :columns="columns" :data-source="props.detailControl.record.dtHydbhxq"  size="small" bordered />
     </a-drawer>
 </template>
 <script setup>
@@ -19,22 +19,58 @@ const onClose = () => {
 };
 
 const columns = [
-  {
-    title: '采样单元序号',
-    dataIndex: 'dtCydyxh',
+{
+    title: '批次煤样标识',
+    dataIndex: 'mybs',
     width: 100,
     align: 'center'
   },
   {
-    title: '采样单元编码',
+    title: '化验单1ID',
     width: 100,
-    dataIndex: 'dtCydybm',
+    dataIndex: 'dtHyd1id',
     align: 'center'
   },
   {
-    title: '采样方式',
+    title: '化验单2ID',
     width: 100,
-    dataIndex: 'dtCyfs',
+    dataIndex: 'dtHyd2id',
+    align: 'center'
+  },
+  {
+    title: '化验单3ID',
+    width: 100,
+    dataIndex: 'dtHyd3id',
+    align: 'center'
+  },
+  {
+    title: '化验单4ID',
+    width: 100,
+    dataIndex: 'dtHyd4id',
+    align: 'center'
+  },
+  {
+    title: '化验单5ID',
+    width: 100,
+    dataIndex: 'dtHyd5id',
+    align: 'center'
+  },
+  {
+    title: '化验单6ID',
+    width: 100,
+    dataIndex: 'dtHyd6id',
+    align: 'center'
+  },
+  {
+    title: '化验单7ID',
+    width: 100,
+    dataIndex: 'dtHyd7id',
+    align: 'center'
+  },
+  {
+    title: '化验单8ID',
+    width: 100,
+    dataIndex: 'dtHyd8id',
     align: 'center'
   }
 ];
