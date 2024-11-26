eae8a854a56897789a6240e72818baf6860c0700
OuYangLiang <ouyanggod@gmail.com>
2024-11-26 22:01:43 +0800

    menu opt for all

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   { path: '/login', component: () => import('../views/LoginView.vue')},
   { path: '/user', component: () => import('../views/user/Page.vue')},
-  { path: '/guidaoheng', component: () => import('../views/guidaoheng/Page.vue')}
+  { path: '/guidaoheng', component: () => import('../views/guidaoheng/Page.vue')},
+  { path: '/qichecheng', component: () => import('../views/qichecheng/Page.vue')},
+  { path: '/meicaiyang', component: () => import('../views/meicaiyang/Page.vue')},
+  { path: '/pidaimeicaiyang', component: () => import('../views/pidaimeicaiyang/Page.vue')},
+  { path: '/meizhi', component: () => import('../views/meizhi/Page.vue')},
+  { path: '/huayandan', component: () => import('../views/huayandan/Page.vue')}
 ]
 
 const router = createRouter({
---
+++ src/views/huayandan/Page.vue
@@ -0,0 +1,52 @@
+<template>
+    <a-layout style="min-height: 100vh">
+        <Menu selectedKeys="huayandan"/>
+
+        <a-layout >
+            <Header />
+
+            <div style="min-height: 80vh">
+                <a-layout-content style="margin: 0 16px">
+                    <a-breadcrumb style="margin: 16px 0">
+                        
+                    </a-breadcrumb>
+                    <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+                        <SearchComp @searchEvent="searchEvent" :formState="formState"/>
+
+                        <a-divider />
+
+                        <TableComp ref="mainTableRef" :searchParam="formState"  />
+                    </div>
+                </a-layout-content>
+                <Detail :detailControl="detailControl" />
+            </div>
+
+            <Footer />
+        </a-layout>
+    </a-layout>
+</template>
+<script setup>
+import Menu from '@/components/Menu.vue';
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import TableComp from '@/views/huayandan/TableComp.vue';
+import SearchComp from '@/views/huayandan/SearchComp.vue';
+import Detail from '@/views/huayandan/Detail.vue';
+
+import { ref, reactive } from 'vue'
+import moment from 'moment';
+const formState = reactive({ "page":1, "pageSize":10, group:'', uploadStatus:'', ywrqRange: [moment().subtract(7, 'days').format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')]})
+
+const detailControl = reactive({"visible":false, "record": {}})
+
+const mainTableRef = ref();
+
+function searchEvent() {
+    mainTableRef.value.search()
+}
+
+function viewDetail(param) {
+    detailControl.record = param;
+    detailControl.visible = true;
+}
+</script>
